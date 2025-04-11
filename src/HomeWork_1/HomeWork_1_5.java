cf76e86c8604b002ec811c669603afb2e58987f7
Alina-ops-dev <alinokz2008@gmail.com>
2025-04-12 00:44:03 +0500

    added HomeWork_1_5

---
+++ src/HomeWork_1/HomeWork_1_5.java
@@ -0,0 +1,10 @@
+package HomeWork_1;
+
+public class HomeWork_1_5 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int z = 7 % b;
+        System.out.println(z);
+    }
+}
