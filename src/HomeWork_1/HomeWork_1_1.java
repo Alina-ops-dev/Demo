70536726d6b1d42a582bd4469f87d3e03a5b651e
Alina-ops-dev <alinokz2008@gmail.com>
2025-04-12 00:12:46 +0500

    added HomeWork_1_1

--- src/HomeWork_1/HomeWork_1_1.java
+++ src/HomeWork_1/HomeWork_1_1.java
@@ -1,4 +1,10 @@
 package HomeWork_1;
 
 public class HomeWork_1_1 {
+        public static void main(String[] args) {
+            int b = 127;
+            int c = 6;
+            int a = 4 * (b + c - 1) / 2;
+            System.out.println(a);
+        }
 }
