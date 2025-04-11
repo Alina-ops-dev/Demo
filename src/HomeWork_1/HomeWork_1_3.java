2f958f5099783e7cc2ce42283e6a20d349e860aa
Alina-ops-dev <alinokz2008@gmail.com>
2025-04-12 00:36:31 +0500

    added HomeWork_1_3

---
+++ src/HomeWork_1/HomeWork_1_3.java
@@ -0,0 +1,12 @@
+package HomeWork_1;
+
+public class HomeWork_1_3 {
+    public static void main(String[] args) {
+        int n = 925;
+        int a = 9;
+        int b = 2;
+        int c = 5;
+        int result = a + b + c;
+        System.out.println(result);
+    }
+}
